3691ca2535d7687530294ce8a3139f54cffd2ea1
pritul dave <41751718+pritul2@users.noreply.github.com>
2022-02-01 19:33:07 +0530

    Beta Version

--- setup_server.py
+++ setup_server.py
@@ -8,6 +8,7 @@ This software is free to reuse
 from aiohttp import web
 import jinja2
 import aiohttp_jinja2
+import os
 
 #Setting up HTML page routing#
 routes = web.RouteTableDef()
@@ -23,5 +24,9 @@ app['static_root_url'] = '/static'
 
 setup_jinja()
 
+ls = os.listdir(".")
 
-
+if not 'static' in ls:
+	os.mkdir("./static")
+	os.mkdir("./static/Files")
+	os.mkdir("./static/Zip_Extracted")
